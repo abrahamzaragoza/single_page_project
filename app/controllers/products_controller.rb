17fd955f082b07eaaa09ed540070a801542bbc69
abrahamzaragoza <abraham.zaragoza@enroutesystems.com>
2023-01-11 12:07:22 -0600

    section 8

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
+
   def index
     @products = Product.all
   end
 
-  def show
-    @product = Product.find(params[:id])
-  end
+  def show; end
 
   def new
     @product = Product.new
@@ -14,19 +14,30 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     @product.save
+    redirect_to root_path
   end
 
-  def edit
-    @product = Product.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @product = Product.find(params[:id])
     @product.update(product_params)
     redirect_to root_path
   end
 
+  def destroy
+    @product.destroy
+    redirect_to root_path
+  end
+
   private
+
+  def find_product
+    begin
+      @product = Product.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_path
+    end
+  end
 
   def product_params
     params.require(:product).permit(:name, :price, :description, :image_url)
